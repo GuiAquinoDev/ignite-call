df977b44ff6e96b98d2ca290a15863e83a89c29a
Guilherme Aquino <guiiaquino92@gmail.com>
2023-02-06 13:14:13 -0300

    feat(time-intervals): criando controllers para o checkbox e validação com o zod

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,26 +1,28 @@
-import { Heading, MultiStep, Text, Button } from '@ignite-ui/react'
-import { ArrowRight, Check } from 'phosphor-react'
-import { Container, Header } from '../styles'
-import { AuthError, ConnectBox, ConnectItem } from './styles'
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { signIn, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import { ArrowRight, Check } from 'phosphor-react'
+// import { api } from "../../../lib/axios"
+import { Container, Header } from '../styles'
+import { AuthError, ConnectBox, ConnectItem } from './styles'
 
 export default function ConnectCalendar() {
   const session = useSession()
   const router = useRouter()
 
   const hasAuthError = !!router.query.error
-  const isSignedIn = session.status === 'authenticated'
+  const isSignedId = session.status === 'authenticated'
 
   async function handleConnectCalendar() {
     await signIn('google')
   }
 
+  console.log(session)
+
   return (
     <Container>
       <Header>
         <Heading as="strong">Conecte sua agenda!</Heading>
-
         <Text>
           Conecte o seu calendário para verificar automaticamente as horas
           ocupadas e os novos eventos à medida em que são agendados.
@@ -31,8 +33,8 @@ export default function ConnectCalendar() {
 
       <ConnectBox>
         <ConnectItem>
-          <Text>Google Calendar!</Text>
-          {isSignedIn ? (
+          <Text>Google Calendar</Text>
+          {isSignedId ? (
             <Button size="sm" disabled>
               Conectado
               <Check />
@@ -44,6 +46,7 @@ export default function ConnectCalendar() {
               onClick={handleConnectCalendar}
             >
               Conectar
+              <ArrowRight />
             </Button>
           )}
         </ConnectItem>
@@ -51,12 +54,12 @@ export default function ConnectCalendar() {
         {hasAuthError && (
           <AuthError size="sm">
             Falha ao se conectar ao Google, verifique se você habilitou as
-            permissôes de acesso ao Google Calendar.
+            permissões de acesso ao Google Calendar
           </AuthError>
         )}
 
-        <Button type="submit" disabled={!isSignedIn}>
-          Próximo Passo
+        <Button type="submit" disabled={!isSignedId}>
+          Próximo passo
           <ArrowRight />
         </Button>
       </ConnectBox>
